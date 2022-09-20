7fa2fea9e6bc409a1476089b59b12874868d7bfe
Sara <sarabonati96@gmail.com>
2022-09-20 14:34:52 +0200

    update pydantic schema to include React viz attributes

--- models/network.py
+++ models/network.py
@@ -14,12 +14,12 @@ class node(BaseModel):
     @validator('node_num')
     def max_ten_nodes(cls, n):
         if n<0 or n>9:
-            raise ValueError('must be a number between 0 and 9')
+            raise ValueError('node number must be a number between 0 and 9')
         return n
     @validator('level')
     def max_four_levels(cls, n):
         if n<0 or n>3:
-            raise ValueError('must be a number between 0 and 3')
+            raise ValueError('level must be a number between 0 and 3')
         return n
 
     class Config:
@@ -37,19 +37,26 @@ class node(BaseModel):
         }
 
 class edge(BaseModel):
-    source_id: int
-    target_id: int
+    source_num: int
+    target_num: int
     reward: int
+    arc_type: str
+    source_x: float
+    source_y: float
+    arc_x: float
+    arc_y: float
+    target_x: float
+    target_y: float
 
-    @validator('source_id')
+    @validator('source_num')
     def check_source(cls, n):
         if n<0 or n>9:
-            raise ValueError('must be a number between 0 and 9')
+            raise ValueError('source node number must be a number between 0 and 9')
         return n
-    @validator('target_id')
+    @validator('target_num')
     def check_target(cls, n):
         if n<0 or n>9:
-            raise ValueError('must be a number between 0 and 9')
+            raise ValueError('target node number must be a number between 0 and 9')
         return n
     @validator('reward')
     def check_reward(cls, n):
@@ -57,21 +64,31 @@ class edge(BaseModel):
         if n not in possible_rewards:
             raise ValueError(f'reward must be a value in {possible_rewards}')
         return n
+    @validator('arc_type')
+    def check_arc_type(cls, n):
+        if n not in ['straight','curved']:
+            raise ValueError('arc type must be chosen among possible options between straight or curved')
+        return n
     @root_validator()
     def validate_no_self_connection(cls, values: Dict[str, Any]) -> Dict[str, Any]:
-         print(Counter(values.get("source_id")))
-         print(Counter(values.get("target_id")))
-         if values.get("source_id") == values.get("target_id"):
-              raise ValueError("source_id must be different from target_id")
+         if values.get("source_num") == values.get("target_num"):
+              raise ValueError("source_num must be different from target_num")
          return values
 
     class Config:
         schema_extra = {
             "example": [
                 {
-                    'source_id': 0,
-                    'target_id': 2,
-                    'reward':20
+                    'source_num': 0,
+                    'target_num': 2,
+                    'reward':20,
+                    'arc_type': 'straight',
+                    'source_x': 89.84720834968113,
+                    'source_y': 7.376435889674515,
+                    'arc_x': 34.54971253634034,
+                    'arc_y': 47.55241560766308,
+                    'target_x': -20.02973382010107,
+                    'target_y': 87.20670189283723
                 }
             ]
         }
