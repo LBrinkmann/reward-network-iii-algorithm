7c6f6938228cfeaa920ac30fe353e5e56038fec0
Sara <sarabonati96@gmail.com>
2022-10-18 13:55:46 +0200

    change observation structure

--- notebooks/environment_vect.py
+++ notebooks/environment_vect.py
@@ -18,6 +18,7 @@ import json
 import random
 import time
 import torch
+import torch.nn.functional as F
 from collections import Counter
 from typing import Optional
 
@@ -52,6 +53,7 @@ class Reward_Network(gym.Env):
         self.INIT_REWARD = 0
         self.INIT_STEP = 0
         self.MAX_STEP = 8
+        self.N_REWARD_IDX = 6
         self.N_NODES = 10
         self.N_NETWORKS = len(self.network)
 
@@ -89,15 +91,18 @@ class Reward_Network(gym.Env):
         # Reset the state of the environment to an initial state
         self.reward_balance = torch.full((self.N_NETWORKS,1),self.INIT_REWARD)
         self.step_counter = torch.full((self.N_NETWORKS,1),self.INIT_STEP)
+        self.big_loss_counter = torch.zeros((self.N_NETWORKS,1),dtype=torch.long)
         self.is_done = False 
         self.current_node = self.starting_nodes.clone()
 
-        print('Environment initialized: \n')
+        print('ENVIRONMENT INITIALIZED:')
         print(f'- set of nodes of shape {self.nodes.shape}')
         print(f'- action space of shape {self.action_space_idx.shape}')
         print(f'- reward balance of shape {self.reward_balance.shape}')
+        print(f'- bog loss counter of shape {self.big_loss_counter.shape}')
         print(f'- step counter of shape {self.step_counter.shape}')
         print(f'- current node of shape {self.current_node.shape}')
+        print('\n')
 
     
     def step(self, action):
@@ -107,22 +112,26 @@ class Reward_Network(gym.Env):
         '''
 
         self.source_node = self.current_node
-        print(f'Source nodes are: {self.current_node}, we are going to new nodes {action}')
+        #print(f'Source nodes are: {self.current_node}, we are going to new nodes {action}')
         
         rewards_idx = torch.unsqueeze(self.action_space_idx[self.network_idx,self.current_node,action], dim=-1)
+        # add to big loss counter if 1 present in rewards_idx
+        self.big_loss_counter= torch.add(self.big_loss_counter,(rewards_idx==1).int())
+
         rewards = self.reward_map[rewards_idx]
         self.reward_balance = torch.add(self.reward_balance,rewards)
-        print(f'We get rewards : {rewards[:,0]} and the new reward balance is: {self.reward_balance[:,0]}')
+        #print(f'We get rewards : {rewards[:,0]} and the new reward balance is: {self.reward_balance[:,0]}')
         self.current_node = action
-        print(f'Now we are in nodes: {self.current_node}')
+        #print(f'Now we are in nodes: {self.current_node}')
         self.step_counter = torch.add(self.step_counter,1)
-        print(f'Step counter for all networks is: {self.step_counter[:,0]}')
-        print('\n')
+        #print(f'Step counter for all networks is: {self.step_counter[:,0]}')
+        #print('\n')
 
         if torch.all(self.step_counter == 8):
             self.is_done = True
 
-        return rewards
+        #return action,rewards,self.is_done
+        return self.observe(),rewards
         
         
     def get_state(self):
@@ -154,13 +163,56 @@ class Reward_Network(gym.Env):
         """
         this function returns observation from the environment
         """
-
-        self.next_nodes = torch.squeeze(torch.unsqueeze(self.edge_is_present[self.network_idx,self.current_node,:],dim=-1))
+        self.observation_matrix = torch.zeros((self.N_NETWORKS,self.N_NODES,(self.N_REWARD_IDX+self.MAX_STEP+1+1)),dtype=torch.long)
         self.next_rewards_idx = torch.squeeze(torch.unsqueeze(self.action_space_idx[self.network_idx,self.current_node,:],dim=-1))
+        
+        # one hot encoding of reward_idx
+        #print(f'one hot encoding of reward idx shape \n')
+        #print(F.one_hot(self.next_rewards_idx,num_classes=self.N_REWARD_IDX).shape)
+        #print(f'example one hot encoding of reward idx for node 3 of all networks \n')
+        #print(self.next_rewards_idx )
+        #print(F.one_hot(self.next_rewards_idx,num_classes=self.N_REWARD_IDX)[:,3,:])
+        self.observation_matrix[:,:,:self.N_REWARD_IDX] = F.one_hot(self.next_rewards_idx,num_classes=self.N_REWARD_IDX)
+        
+        # one hot encoding of step counter TODO: n_steps or (n_steps+1)
+        #print(f'one hot encoding of step counter \n')
+        #print(F.one_hot(self.step_counter,num_classes=self.MAX_STEP+1))
+        self.observation_matrix[:,:,self.N_REWARD_IDX:(self.N_REWARD_IDX+self.MAX_STEP+1)] = torch.repeat_interleave(F.one_hot(self.step_counter,num_classes=self.MAX_STEP+1), self.N_NODES,dim=1)
 
-        return {'current_node':self.current_node,
-                'next_possible_nodes':self.next_nodes,
-                'next_possible_rewards_idx':self.next_rewards_idx,
-                'total_reward':self.reward_balance,
-                'n_steps':self.step_counter,
-                'done':self.is_done}
+        # big loss counter
+        #print(f'big loss counter \n')
+        #print(self.big_loss_counter.shape)
+        #print(f'big loss counter repeated to fit 10 nodes \n')
+        #print(torch.repeat_interleave(self.big_loss_counter, self.N_NODES,dim=1))
+        #print(f'big loss counter repeated to fit 10 nodes shape \n')
+        #print(torch.repeat_interleave(self.big_loss_counter, self.N_NODES,dim=1).shape)
+        self.observation_matrix[:,:,-1] = torch.repeat_interleave(self.big_loss_counter, self.N_NODES,dim=1)
+        #print(f'shape of main observation matrix: {self.observation_matrix.shape}')
+        
+        # the second observation matrix (boolean mask indicating valid actions)
+        self.next_nodes = torch.squeeze(torch.unsqueeze(self.edge_is_present[self.network_idx,self.current_node,:],dim=-1))
+        
+        return {'mask':self.next_nodes,
+                'obs':self.observation_matrix}
+        #return {'current_node':self.current_node,
+        #        'next_possible_nodes':self.next_nodes,
+        #        'next_possible_rewards_idx':self.next_rewards_idx,
+        #        'total_reward':self.reward_balance,
+        #        'big_loss_counter':self.big_loss_counter,
+        #        'n_steps':self.step_counter,
+        #        'done':self.is_done}
+
+
+# For quick testing purposes, comment out if not needed
+
+# with open(os.path.join(os.getcwd(),'data','train.json')) as json_file:
+#     test = json.load(json_file)
+# env_test = Reward_Network(test[10:13])
+
+
+# env_test.reset()
+# print(env_test.observe())
+# print('\n')
+# action,rewards,done = env_test.step(torch.tensor([3,6,2]))
+# print('\n')
+# print(env_test.observe())
