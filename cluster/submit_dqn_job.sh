28b43dd857ea7d66bfbfc3e62423fd39de124dc4
Sara <sarabonati96@gmail.com>
2022-10-31 11:15:31 +0100

    update SLURM job submission script

--- cluster/submit_dqn_job.sh
+++ cluster/submit_dqn_job.sh
@@ -2,9 +2,9 @@
 
 # This script parallelizes the jobs related to the Reward Networks project on the MPIB Cluster (tardis)
 # The process is executed on tardis using a job wrapper for SLURM resource management system.
-# 
-# Overview of methods:
-# - dqn: calls DQN agent to be trained on solving reward networks 
+# There are two possible "methods" in the script:
+# - dqn: this trains the DQN agent but does NOT run hyperparameter tuning
+# - tune: this trains the DQN agent AND runs hyperparameter tuning
 #
 # PROJECT: Reward Networks III
 # author @ Sara Bonati
@@ -16,18 +16,17 @@ method="dqn"
 
 # -------Directory and filename (fn) management-------------------------------
 home_dir="/mnt/beegfs/home/bonati"
-project_dir="${home_dir}/CHM/reward_network"
-code_dir="${project_dir}/reward-network-iii-algorithm"
+project_dir="${home_dir}/CHM/reward_networks_III"
+code_dir="${project_dir}/reward-network-iii-algorithm/notebooks"
 data_dir="${project_dir}/data"
+out_dir="${project_dir}/results"
+log_dir="${project_dir}/logs"
 
+# script path
+main_script_fn=${code_dir}/notebooks/dqn_agent.py
+# virtual environment path
+venv_path="../.venv/bin/activate"
 
-# output parent directories
-if [[ "${method}" == "dqn" ]]; then
-  output_dir="${data_dir}/solutions"
-  log_dir="${project_dir}/logs"
-  # script path
-  main_script_fn=${code_dir}/notebooks/dqn_agent.py
-fi
 
 # Create directories if non-existent
 if [[ ! -d "${output_dir}" ]]; then
@@ -40,20 +39,23 @@ if [ ! -d "${log_dir}/${method}" ]; then
   mkdir -p "${log_dir}/${method}"
 fi
 
-# -------Define computation parameters------------------------
-n_cpus=1 # maximum number of cpus per process
-mem=7GB # memory demand
-device="gpu"
-cuda_specs="gpu:pascal:1"
+
+# --------Create SLURM job and submit to cluster----------------------
 
 if [[ "${method}" == "dqn" ]]; then
+
+  # Define computation parameters
+  n_cpus=4 # maximum number of cpus per process
+  mem=10GB # memory demand
+  device="cpu"#"gpu"
+  cuda_specs="gpu:pascal:1"
 
   # Create slurm job file
   echo "#!/bin/bash" >job.slurm
   # Name job file
   echo "#SBATCH --job-name reward_networks-${method}" >>job.slurm
   # Specify maximum run time
-  echo "#SBATCH --time 24:00:00" >>job.slurm
+  echo "#SBATCH --time 10:00:00" >>job.slurm
   # Request cpus
   echo "#SBATCH --cpus-per-task ${n_cpus}" >>job.slurm
   # Specify RAM for operation
@@ -69,9 +71,9 @@ if [[ "${method}" == "dqn" ]]; then
   # Activate virtual environment on cluster
   echo "source .venv/bin/activate" >>job.slurm
   # Call main python script
-  echo "python3 ${main_script_fn} ${method}" >>job.slurm
-  echo "python3 ${main_script_fn} ${method}"
-  # Submit job to cluster and remove job
+  echo "python3 ${main_script_fn} ${data_dir} ${out_dir}" >>job.slurm
+  echo "python3 ${main_script_fn} ${data_dir} ${out_dir}"
+  # Submit job to cluster and remove slurm file
   echo "REWARD NETWORKS ${method} in queue"
   echo ""
 
@@ -80,3 +82,43 @@ if [[ "${method}" == "dqn" ]]; then
 
 fi
 
+
+if [[ "${method}" == "tune" ]]; then
+  
+  # Define computation parameters
+  n_cpus=4 # maximum number of cpus per process
+  mem=10GB # memory demand
+  device="gpu"
+  cuda_specs="gpu:pascal:1"
+
+  # Create slurm job file
+  echo "#!/bin/bash" >job.slurm
+  # Name job file
+  echo "#SBATCH --job-name reward_networks-${method}" >>job.slurm
+  # Specify maximum run time
+  echo "#SBATCH --time 10:00:00" >>job.slurm
+  # Request cpus
+  echo "#SBATCH --cpus-per-task ${n_cpus}" >>job.slurm
+  # Specify RAM for operation
+  echo "#SBATCH ---mem-per-cpu ${mem}" >>job.slurm
+  # Specify partition (gpu)
+  echo "#SBATCH ---partition ${device}" >>job.slurm
+  # Specify CUDA GPU
+  echo "#SBATCH ---gres ${cuda_specs}" >>job.slurm
+  # Write output log to log directory
+  echo "#SBATCH --output ${log_dir}/${method}/reward_networks-${method}_%j_$(date '+%Y_%m-%d_%H-%M-%S').out" >>job.slurm
+  # Start in current directory:
+  echo "#SBATCH --workdir ${code_dir}" >>job.slurm
+  # Activate virtual environment on cluster
+  echo "source .venv/bin/activate" >>job.slurm
+  # Call main python script
+  echo "python3 ${main_script_fn} -t ${data_dir} ${out_dir}" >>job.slurm
+  echo "python3 ${main_script_fn} -t ${data_dir} ${out_dir}"
+  # Submit job to cluster and remove slurm file
+  echo "REWARD NETWORKS ${method} in queue"
+  echo ""
+
+  sbatch job.slurm
+  rm -f job.slurm
+
+fi
