46064959100ab3ee443282b90878bb270f51e35c
Sara <sarabonati96@gmail.com>
2022-12-08 16:41:36 +0100

    specify torch device

--- dqn/nn.py
+++ dqn/nn.py
@@ -3,9 +3,10 @@ import torch.nn.functional as F
 
 
 class DQN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self, input_size, output_size, hidden_size, device):
         super(DQN, self).__init__()
 
+        self.device = device
         self.linear1 = nn.Linear(input_size[-1], hidden_size[-1])
         self.linear2 = nn.Linear(hidden_size[-1], output_size[-1])
 
@@ -16,4 +17,4 @@ class DQN(nn.Module):
         x = F.relu(self.linear1(i))
         # q = F.relu(self.linear2(x))
         q = self.linear2(x)
-        return q
+        return q.to(self.device)
